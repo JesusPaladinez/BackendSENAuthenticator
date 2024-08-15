2e09d06772ffc4126e8ec2430f221b3c1ba2030d
Jesús Paladinez <jesuspaladinez18@gmail.com>
2024-08-14 19:11:59 -0500

    API de Autenticación Facial

--- app_senauthenticator/serializers/autenticacion_facial.py
+++ app_senauthenticator/serializers/autenticacion_facial.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
 
 
-class RostroSerializer(serializers.Serializer):
+class AutenticacionFacial(serializers.Serializer):
     current_face = serializers.CharField()
