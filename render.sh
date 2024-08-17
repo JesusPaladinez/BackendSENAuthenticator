8d6f7c4eb43f1d6d952ad99177b85fe866815718
Jesús Paladinez <jesuspaladinez18@gmail.com>
2024-08-16 21:00:28 -0500

    Actualización de setuptools

--- render.sh
+++ render.sh
@@ -1,5 +1,5 @@
-# Actualiza pip a la última versión
-pip install --upgrade pip
+# Actualiza pip y setuptools a la última versión
+pip install --upgrade pip setuptools
 
 # Instala las dependencias desde requirements.txt
 pip install -r requirements.txt
