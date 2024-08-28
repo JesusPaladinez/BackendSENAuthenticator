00d1bceb8bb8a62264f249254c6a9776edfd72b7
Jesús Paladinez <jesuspaladinez18@gmail.com>
2024-08-27 21:11:32 -0500

    Se actualizó el archivo con los comandos de construcción

--- render.sh
+++ render.sh
@@ -1,6 +1,8 @@
 # Actualiza pip y setuptools a la última versión
 pip install --upgrade pip setuptools
 
-# Instala las dependencias desde requirements.txt, migra a la base de datos y carga los estilos
-pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput
+# Instala las dependencias del sistema necesarias para compilar dlib y otras librerías
+apt-get update && apt-get install -y build-essential cmake
 
+# Instala las dependencias desde requirements.txt, migra la base de datos y carga los estilos
+pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput
