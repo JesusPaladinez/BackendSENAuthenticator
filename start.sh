4557cbd9e821587268abacc380d42bcd57971540
Jesús Paladinez <jesuspaladinez18@gmail.com>
2024-09-22 00:19:30 -0500

    Actualización del start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Imprime el puerto para verificar que está configurado correctamente
+echo "Starting application on port: $PORT"
+
 # Establece el puerto por defecto si no está definido
 PORT=${PORT:-8080}
 
-# Recoger los archivos estáticos 
+# Aplica las migraciones
+/opt/venv/bin/python /Backend/manage.py migrate --no-input
+
+# Recoge los archivos estáticos
 /opt/venv/bin/python /Backend/manage.py collectstatic --no-input
 
-# Iniciar el servidor Gunicorn con la configuración adecuada
-exec /opt/venv/bin/gunicorn proyecto_senauthenticator.wsgi:application --bind 0.0.0.0:$PORT --workers 3
+# Inicia el servidor Gunicorn con configuración de tiempo de espera ajustada
+exec /opt/venv/bin/gunicorn proyecto_senauthenticator.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 120
