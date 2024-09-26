043ad63d7513331300503d01fb34f6a621e0b6b2
Miguel Angel <miguelangelastudillo38@gmail.com>
2024-09-26 09:03:55 -0500

    Algunos cambios

--- proyecto_senauthenticator/settings.py
+++ proyecto_senauthenticator/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from decouple import config
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,12 +88,20 @@ WSGI_APPLICATION = 'proyecto_senauthenticator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL')
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
 }
 
+DATABASES['default']=dj_database_url.parse(config("DATABASE_URL"))
 
 
 # DATABASES['default']=dj_database_url.parse("postgresql://postgres:fOpxDpFkMmJlCrlASPofRhEiBkKiAqmU@postgres.railway.internal:5432/railway")
